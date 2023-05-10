c6410b31ac5bc3f5aa3f19bdf10125c56247ceb3
Syeda <syedashamaila1721@gmail.com>
2023-05-10 20:50:10 +0530

    Added PropertyList component

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,5 +1,5 @@
 import { style } from "@angular/animations";
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector:'app-property-card',
@@ -11,11 +11,5 @@ import { Component } from "@angular/core";
   styleUrls:['property-card.component.css']
 })
 export class PropertyCardComponent{
-  Property : any = {
-    Id : 1,
-    Name : "Birla House",
-    Type : "House",
-    Price : 12000
-
- }
+  @Input() property: any
 }
--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+  properties : Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Erose Villa",
+      "Type":"Villa",
+      "Price":14000
+    },
+    {
+      "Id":3,
+      "Name":"Mark Hill",
+      "Type":"Flat",
+      "Price":12500
+    },
+    {
+      "Id":4,
+      "Name":"Gun Hill",
+      "Type":"Duplex",
+      "Price":11400
+    },
+    {
+      "Id":5,
+      "Name":"Macro Home",
+      "Type":"House",
+      "Price":14300
+    },
+    {
+      "Id":6,
+      "Name":"Pearl White House",
+      "Type":"House",
+      "Price":18000
+    }
+]
+
+ constructor(){}
+ ngOnInit(): void {
+
+ }
 
 }
